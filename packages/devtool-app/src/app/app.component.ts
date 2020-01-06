320b428d9922c1190add9f791843a930cdb46385
Andrey <geass1000@gmail.com>
2020-01-06 04:08:43 +0300

    feat(pkg/app): add example of connecting to chrome background script

--- packages/devtool-app/src/app/app.component.ts
+++ packages/devtool-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import * as Core from '@krix-devtool/core';
 
 @Component({
   selector: 'krix-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DevTool';
+
+  ngOnInit (): void {
+    if (_.isNil(chrome)) {
+      return;
+    }
+
+    console.log(`DTA: Hello World!`);
+
+    const port = chrome.runtime.connect({
+      name: Core.Constants.DTAToBgSConnectionName,
+    });
+
+    port.onMessage.addListener((message, senderPort) => {
+      console.log(`DTA - onMessage:`, message, senderPort);
+    });
+
+    port.postMessage({
+      payload: `Hi, I'm a DevTool Application!`,
+    });
+  }
 }
