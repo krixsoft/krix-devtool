e5a8727f7cd6820521bb97c345790dad299a8967
Andrey <geass1000@gmail.com>
2020-01-06 04:06:28 +0300

    feat(pkg/core): add `Connection Name` constants

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,2 +1,11 @@
+import * as Enums from './enums';
 
 export const AppName: 'Krix-DevTool' = `Krix-DevTool`;
+
+const EndpointSeparator: string = `_`;
+
+export const CSToBgSConnectionName: string = Enums.AppEndpoint.ContentScript
+  + EndpointSeparator + Enums.AppEndpoint.BackgroundScript;
+
+export const DTAToBgSConnectionName: string = Enums.AppEndpoint.DevToolApp
+  + EndpointSeparator + Enums.AppEndpoint.BackgroundScript;
