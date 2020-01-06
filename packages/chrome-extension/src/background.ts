0cb0185a738f5d36e52d50933e9377e0f72018cd
Andrey <geass1000@gmail.com>
2020-01-06 04:10:07 +0300

    feat(pkg/c-ext): add handler of connection into the background script

--- packages/chrome-extension/src/background.ts
+++ packages/chrome-extension/src/background.ts
@@ -1,1 +1,10 @@
-console.log(`Hello World!`);
+console.log(`BgS: Hello World!`);
+
+chrome.runtime.onConnect.addListener((port) => {
+  console.log(`BgS - onConnect:`, port);
+
+  port.onMessage.addListener((message, senderPort) => {
+    console.log(`BgS - onMessage:`, message, senderPort);
+    port.postMessage({ payload: `Hello!` });
+  });
+});
