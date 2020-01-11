cd03358721cd5f2f1e36667ace70d9c9707a7f61
Andrey <geass1000@gmail.com>
2020-01-11 05:05:37 +0300

    feat(pkg/c-ext): add `Message Retranslator` logic to CS

---
+++ packages/chrome-extension/src/contentscript/message-retranslator.ts
@@ -0,0 +1,35 @@
+import * as Core from '@krix-devtool/core';
+
+export class MessageRetranslator extends Core.Singleton {
+  private bgsPort: chrome.runtime.Port;
+  private id: number;
+
+  setBgSPort (port: chrome.runtime.Port): void {
+    this.bgsPort = port;
+  }
+
+  setIdentifier (id: number): void {
+    this.id = id;
+  }
+
+  sendMessage <TMsg = any> (
+    endpoint: Core.Enums.AppEndpoint,
+    msgType: any,
+    msgData?: TMsg,
+  ): void {
+    if (endpoint === Core.Enums.AppEndpoint.ContentScript) {
+      console.warn(`MessageRetranslator - sendMessage:`,
+        `CS is trying to send messages to the unsupported endpoint (${endpoint})`);
+      return;
+    }
+
+    const messsag: Core.Interfaces.BaseMessage<TMsg> = {
+      id: this.id,
+      ept: endpoint,
+      type: msgType,
+      payload: msgData,
+    };
+
+    this.bgsPort.postMessage(messsag);
+  }
+}
