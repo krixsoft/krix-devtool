4c49d726ac1ea571d7994e3fca43780eaeb76ba3
Andrey <geass1000@gmail.com>
2020-01-11 05:03:36 +0300

    feat(pkg/app): use `Message Retranslator` service to send `Init` message to BgS

--- packages/devtool-app/src/app/app.component.ts
+++ packages/devtool-app/src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import * as _ from 'lodash';
+import { Inject, Component, OnInit } from '@angular/core';
+
 import * as Core from '@krix-devtool/core';
+
+import * as Shared from './shared';
+
+// Services
+import {
+  MessageRetranslatorService,
+} from './core/services';
 
 @Component({
   selector: 'krix-root',
@@ -10,30 +17,38 @@ import * as Core from '@krix-devtool/core';
 export class AppComponent implements OnInit {
   title = 'DevTool';
 
+  constructor (
+    private readonly messageRetranslator: MessageRetranslatorService,
+    @Inject(Shared.Constants.DI.Lodash)
+    private readonly lodash: Shared.Interfaces.Pkg.Lodash,
+  ) {
+  }
+
   ngOnInit (): void {
-    if (_.isNil(chrome)) {
+    if (this.lodash.isNil(chrome)) {
       return;
     }
     console.log(chrome);
 
     console.log(`DTA: Hello World!`);
 
+    // TODO: Move to BridgeConnectorService
     const port = chrome.runtime.connect({
       name: Core.Constants.DTAToBgSConnectionName,
     });
+    this.messageRetranslator.setBgSPort(port);
 
+    const tabId = this.lodash.get(chrome, `devtools.inspectedWindow.tabId`);
+    this.messageRetranslator.setIdentifier(tabId);
+
+    // TODO: Move to BridgeConnectorService
     port.onMessage.addListener((message, senderPort) => {
       console.log(`DTA - onMessage:`, message, senderPort);
     });
 
-    const initMsg: Core.Interfaces.BaseMessage = {
-      id: chrome.devtools.inspectedWindow.tabId,
-      ept: Core.Enums.AppEndpoint.BackgroundScript,
-      type: Core.Enums.MsgCommands.DevToolApp.InitDevTool,
-      payload: {
-      },
-    };
-
-    port.postMessage(initMsg);
+    this.messageRetranslator.sendMessage(
+      Core.Enums.AppEndpoint.BackgroundScript,
+      Core.Enums.MsgCommands.DevToolApp.InitDevTool,
+    );
   }
 }
