f7b0920be110686442ba21ce75c9eb7f7f0a3824
Daniil <daniil_agaev@smartexlab.com>
2020-04-14 19:50:58 +0300

    feat(pkg/dta): add layout and logic to state-tree component

--- packages/devtool-app/src/app/state-store/state-store-view/state-tree/state-tree.component.ts
+++ packages/devtool-app/src/app/state-store/state-store-view/state-tree/state-tree.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Subscription } from 'rxjs';
+import { HistoryService } from './../../core/services/history.service';
+import { Component, OnInit } from '@angular/core';
+
+import * as _ from 'lodash';
 
 @Component({
-  selector: "krix-state-tree",
-  templateUrl: "./state-tree.component.html",
+  selector: 'krix-state-tree',
+  templateUrl: './state-tree.component.html',
 })
 export class StateTreeComponent implements OnInit {
-  constructor() {}
+  public state: any = {};
+  public sjStoreChange: Subscription;
 
-  ngOnInit() {}
+  constructor (private historyService: HistoryService) { }
+
+  ngOnInit (): void {
+    this.state = this.historyService.getStore();
+
+    this.sjStoreChange = this.historyService
+      .getStoreObserver()
+      .subscribe(() => {
+        this.state = this.historyService.getStore();
+      });
+  }
+
 }
