bf7152f99b9e7db95ca0c5088a2517b2d7874bde
Daniil <daniil_agaev@smartexlab.com>
2020-04-15 16:01:59 +0300

    tml(pkg/dta): tmp commit

--- packages/devtool-app/src/app/state-store/state-store-view/state-tree/state-tree-node/state-tree-node.component.ts
+++ packages/devtool-app/src/app/state-store/state-store-view/state-tree/state-tree-node/state-tree-node.component.ts
@@ -15,6 +15,7 @@ export class StateTreeNodeComponent implements OnInit {
   @Input('state') set inState (value: any) {
     this.state = value;
     this.stateWithOptions = this.transform(this.state);
+    console.log('this.stateWithOptions: ', this.stateWithOptions);
   }
 
   public faMinus = faMinus;
@@ -30,6 +31,11 @@ export class StateTreeNodeComponent implements OnInit {
     stateNode.nodeIsOpen = !stateNode.nodeIsOpen;
   }
 
+  checkNodeValueType (value: any): string {
+    if (_.isArray(value)) { return ' [ ... ] '; }
+    if (_.isObject(value)) { return ' { ... } '; }
+  }
+
   getStateNodeValue (value: any): string {
     if (_.isObject(value)) { return null; }
     if (_.isString(value)) { return ` "${value}"`; }
@@ -42,7 +48,7 @@ export class StateTreeNodeComponent implements OnInit {
   transform (value: any): any {
     if (!_.isObject(value)) { return; };
 
-    const content: { key: string; value: any; nodeIsOpen?: boolean; nodeCanOpen?: boolean }[] = [];
+    const content: { key: string; value: any; nodeIsOpen?: boolean; nodeCanOpen?: boolean; id: number }[] = [];
 
     for (const key in value) {
       if (Object.prototype.hasOwnProperty.call(value, key) === false) {
@@ -52,17 +58,20 @@ export class StateTreeNodeComponent implements OnInit {
       const previousState = _.find(this.stateWithOptions, ['key', key]);
 
       if (_.isNil(previousState) === false) {
-        const newState = _.assign({}, previousState, { value: value[key] });
+        const newState = previousState.value === value[key]
+          ? previousState
+          : _.assign({}, previousState, { value: value[key], id: Date.now() });
+        console.log(newState);
         content.push(newState);
         continue;
       }
 
       if (_.isObject(value[key])) {
-        content.push({ key: key, value: value[key], nodeCanOpen: true, nodeIsOpen: false });
+        content.push({ key: key, value: value[key], nodeCanOpen: true, nodeIsOpen: false, id: Date.now() });
         continue;
       }
 
-      content.push({ key: key, value: value[key], nodeCanOpen: false, nodeIsOpen: false });
+      content.push({ key: key, value: value[key], nodeCanOpen: false, nodeIsOpen: false, id: Date.now() });
     }
     return content;
   }
@@ -72,5 +81,10 @@ export class StateTreeNodeComponent implements OnInit {
     if (_.isNumber(value)) { return '#357bc3'; }
     if (_.isString(value)) { return '#4da23e'; }
     if (_.isBoolean(value)) { return '#d03131'; }
+  }
+
+  trackByFn (index: number, item: any): string {
+    console.log(`${item.key}-${item.id}`);
+    return `${item.key}-${item.id}`;
   }
 }
