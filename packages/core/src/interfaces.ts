d4458f8bae8a4e70592c25a9e9edb3644e8544da
Andrey <geass1000@gmail.com>
2020-05-24 21:49:15 +0300

    feat(pkg/core): add `InitDeps` and `OnInit` interfaces

--- packages/core/src/interfaces.ts
+++ packages/core/src/interfaces.ts
@@ -1,6 +1,14 @@
 import * as Enums from './enums';
 
-export interface BaseMessage<PayloadType = any> {
+export interface OnInit {
+  onInit(): void;
+}
+
+export interface InitDeps {
+  initDeps(): void;
+}
+
+export interface BaseMessage <PayloadType = any> {
   tabId: number;
   endpoint: Enums.AppEndpoint;
   command: Enums.MsgCommands;
