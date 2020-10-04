d50729baff41523e1c57468d5333ffa26507cbba
Andrey <geass1000@gmail.com>
2020-10-04 03:06:20 +0300

    feat(pkg/d-a): add `valueType` field to `StateNode` interface

--- packages/devtool-app/src/app/shared/interfaces.ts
+++ packages/devtool-app/src/app/shared/interfaces.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import * as Enums from './enums';
 
 export namespace Pkg {
   export type Lodash = _.LoDashStatic;
@@ -7,6 +8,7 @@ export namespace Pkg {
 export interface StateNode {
   key: string;
   value: any;
+  valueType: Enums.ValueTypes;
   nodeIsOpened: boolean;
   nodeCanBeOpened: boolean;
 }
