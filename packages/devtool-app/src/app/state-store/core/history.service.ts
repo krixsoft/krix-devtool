a89dc086425fe8fd79b4d0d111f58049447fe87b
Andrey <geass1000@gmail.com>
2020-10-08 00:55:18 +0300

    docs(pkg/dta): add comments to code of `History` service

--- packages/devtool-app/src/app/state-store/core/history.service.ts
+++ packages/devtool-app/src/app/state-store/core/history.service.ts
@@ -9,13 +9,13 @@ import { Interfaces } from '../shared';
 export class HistoryService {
   private krixStateStore: KrixStateStore.StateStore;
 
-  private history: Interfaces.HistoryItem[];
+  private historyList: Interfaces.HistoryItem[];
   private currentHistoryItemIndex: number;
   private currentHistoryItem: Interfaces.HistoryItem;
 
   private sjHistoryChange: Subject<null>;
 
-  private historyMessageNumber: number;
+  private freeHistoryMessageNumber: number;
 
   constructor (
   ) {
@@ -23,31 +23,60 @@ export class HistoryService {
 
     this.sjHistoryChange = new Subject();
 
-    this.history = [];
+    this.historyList = [];
     this.currentHistoryItemIndex = -1;
-    this.historyMessageNumber = 0;
+    this.freeHistoryMessageNumber = 0;
   }
 
+  /**
+   * Returns a history change observer (RxJS Observable).
+   *
+   * @return {Observable<null>}
+   */
   getHistoryChangeObserver (
   ): Observable<null> {
     return this.sjHistoryChange.asObservable();
   }
 
+  /**
+   * Returns an instance of state store.
+   *
+   * @return {KrixStateStore.StateStore}
+   */
   getStateStore (
   ): KrixStateStore.StateStore {
     return this.krixStateStore;
   }
 
+  /**
+   * Returns the list of history items (actions).
+   *
+   * @return {Interfaces.HistoryItem[]}
+   */
   getHistory (
   ): Interfaces.HistoryItem[] {
-    return [ ...this.history ];
+    return [ ...this.historyList ];
   }
 
+  /**
+   * Returns the selected history item.
+   *
+   * @return {Interfaces.HistoryItem}
+   */
   getCurrentHistoryItem (
   ): Interfaces.HistoryItem {
     return this.currentHistoryItem;
   }
 
+  /**
+   * Handles a new state-store command message.
+   * - create a new history item from the new command message.
+   * - increases the free history message number.
+   * - adds the history item to history queue.
+   *
+   * @param  {KrixStateStore.Interfaces.StoreCommand} message
+   * @return {void}
+   */
   onMessage (
     message: KrixStateStore.Interfaces.StoreCommand,
   ): void {
@@ -56,16 +85,20 @@ export class HistoryService {
     }
 
     const historyItem: Interfaces.HistoryItem = _.assign({
-      id: this.historyMessageNumber,
+      id: this.freeHistoryMessageNumber,
     }, message.data);
-    this.historyMessageNumber += 1;
+    this.freeHistoryMessageNumber += 1;
 
-    this.history.push(historyItem);
+    this.historyList.push(historyItem);
 
-    if (this.history.length - 2 === this.currentHistoryItemIndex) {
+    // FYI: If we subtract 1, we get the last element in the queue but the last element is the new element
+    // so we subtract 2 and get the previous last element (when the queue was without the new element).
+    // If the current history item index is the previous last element we will move selector to the next
+    // element.
+    if (this.historyList.length - 2 === this.currentHistoryItemIndex) {
       this.goToNextDirection(1);
       this.currentHistoryItemIndex += 1;
-      this.currentHistoryItem = this.history[this.currentHistoryItemIndex];
+      this.currentHistoryItem = this.historyList[this.currentHistoryItemIndex];
     }
 
     this.sjHistoryChange.next(null);
@@ -80,7 +113,7 @@ export class HistoryService {
   goToHistoryItem (
     newCurrentHistoryItemId: number,
   ): void {
-    const newCurrentHistoryItemIndex = _.findIndex(this.history, [ 'id', newCurrentHistoryItemId ]);
+    const newCurrentHistoryItemIndex = _.findIndex(this.historyList, [ 'id', newCurrentHistoryItemId ]);
     if (newCurrentHistoryItemIndex === this.currentHistoryItemIndex) {
       return;
     }
@@ -94,7 +127,7 @@ export class HistoryService {
     }
 
     this.currentHistoryItemIndex = newCurrentHistoryItemIndex;
-    this.currentHistoryItem = this.history[newCurrentHistoryItemIndex];
+    this.currentHistoryItem = this.historyList[newCurrentHistoryItemIndex];
 
     this.sjHistoryChange.next(null);
   }
@@ -112,7 +145,12 @@ export class HistoryService {
   ) {
     for (let i = 1; i <= stepsCount; i++) {
       const nextHistoryItemIndex = this.currentHistoryItemIndex + i;
-      const nextHistoryItem = this.history[nextHistoryItemIndex];
+      const nextHistoryItem = this.historyList[nextHistoryItemIndex];
+
+      if (nextHistoryItem?.options?.signal === true) {
+        continue;
+      }
+
       this.krixStateStore.setState({
         state: nextHistoryItem.state,
         value: nextHistoryItem.newValue,
@@ -133,7 +171,12 @@ export class HistoryService {
   ) {
     for (let i = 0; i < stepsCount; i++) {
       const prevHistoryItemIndex = this.currentHistoryItemIndex - i;
-      const prevHistoryItem = this.history[prevHistoryItemIndex];
+      const prevHistoryItem = this.historyList[prevHistoryItemIndex];
+
+      if (prevHistoryItem?.options?.signal === true) {
+        continue;
+      }
+
       this.krixStateStore.setState({
         state: prevHistoryItem.state,
         value: prevHistoryItem.oldValue,
@@ -141,7 +184,12 @@ export class HistoryService {
     }
 
     const newCurrentHistoryItemIndex = this.currentHistoryItemIndex - stepsCount;
-    const newCurrentHistoryItem = this.history[newCurrentHistoryItemIndex];
+    const newCurrentHistoryItem = this.historyList[newCurrentHistoryItemIndex];
+
+    if (newCurrentHistoryItem?.options?.signal === true) {
+      return;
+    }
+
     this.krixStateStore.setState({
       state: newCurrentHistoryItem.state,
       value: newCurrentHistoryItem.newValue,
