7c34dba62b7bc950fc17068084421de2cd5fd28f
Andrey <geass1000@gmail.com>
2021-02-14 03:43:19 +0300

    feat(pkg/dta): use `State Store` arbiter to get list of stores and update active store in `SS Store Selector` component

--- packages/devtool-app/src/app/state-store/components/tab-sidebar/store-selector/store-selector.ts
+++ packages/devtool-app/src/app/state-store/components/tab-sidebar/store-selector/store-selector.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 import { BaseComponent } from '../../../../shared/base.component';
 
-import { StateStoreHistoryService } from '../../../core/ss-history.service';
-import { StateStoreMessageHandler } from '../../../core/ss-message-handler';
+import { StateStoreArbiter } from '../../../core/ss.arbiter';
 
 @Component({
   selector: 'krix-store-selector',
@@ -11,19 +10,18 @@ import { StateStoreMessageHandler } from '../../../core/ss-message-handler';
 })
 export class StoreSelectorComponent extends BaseComponent implements OnInit {
   public stateStoresNames: string[];
+  public selectedStateStore: string;
 
-  private selectedStateStoreName: string;
-  set vSelectedStateStoreName (value: string) {
-    console.log(value);
-    this.selectedStateStoreName = value;
+  set fcSelectedStateStoreName (value: string) {
+    this.selectedStateStore = value;
+    this.ssArbiter.setActiveStore(value);
   }
-  get vSelectedStateStoreName (): string {
-    return this.selectedStateStoreName;
+  get fcSelectedStateStoreName (): string {
+    return this.selectedStateStore;
   }
 
   constructor (
-    private ssHistoryService: StateStoreHistoryService,
-    private ssMessageHandler: StateStoreMessageHandler,
+    private ssArbiter: StateStoreArbiter,
     private changeDetection: ChangeDetectorRef,
   ) {
     super();
@@ -32,7 +30,7 @@ export class StoreSelectorComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit (): void {
-    const ssMessageHandler$ = this.ssMessageHandler.getCommandObserver()
+    const ssMessageHandler$ = this.ssArbiter.getObserver()
       .subscribe(() => {
         this.updateView();
       });
@@ -42,7 +40,8 @@ export class StoreSelectorComponent extends BaseComponent implements OnInit {
 
   updateView (
   ): void {
-    this.stateStoresNames = this.ssMessageHandler.getStateStoresNames();
+    this.stateStoresNames = this.ssArbiter.getStoreNames();
+    this.selectedStateStore = this.ssArbiter.getActiveStoreName();
     this.changeDetection.detectChanges();
   }
 }
